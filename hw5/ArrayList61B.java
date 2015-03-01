181246b3a517991197bcc62eb033731b73c6fc75
yifeng96 <yifeng@berkeley.edu>
2015-03-01 01:24:29 -0800

    Completed hw5

--- hw5/ArrayList61B.java
+++ hw5/ArrayList61B.java
@@ -1,27 +1,34 @@
  import java.util.AbstractList;
 
- public class ArrayList61B<E>{
-E [] items;
-int size;
+ public class ArrayList61B<E> extends AbstractList<E>{
+public E [] items;
+public int size=0;
+public int capacity;
 public ArrayList61B(int initialCapacity){
 if(initialCapacity<1){
 	 throw new IllegalArgumentException();
 }
-else{size=initialCapacity;}
+else{capacity=initialCapacity;}
+E[] items = (E[]) new Object[capacity];
 }
 public ArrayList61B(){
-	size=1;
+	capacity=1;
+	E[] items = (E[]) new Object[1];
 }
 public E get(int i) {
+if (i<0||i>=size) {
+	throw new IllegalArgumentException();
+}
 return items[i];
 }
  public boolean add(E item) {
-// what position does the 0th insert go into? position 0
-if (size == items.length) {
+if (item!=null&&capacity>=size){
+if (capacity == size) {
 resize((int) (size * 2));
+capacity=2*capacity;
 }
-items[size] = item;
-size = size + 1;
+items [size] =(E) item;
+size = size + 1;}
 return true;
 }
 public int size(){
@@ -29,7 +36,7 @@ public int size(){
 }
  private void resize(int c) {
 E[] newItems = (E[]) new Object[c];
-for (int i = 0; i < items.length; i += 1) {
+for (int i = 0; i < capacity; i += 1) {
 newItems[i] = items[i];
 }
 items = newItems;
