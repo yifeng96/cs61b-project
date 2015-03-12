de6a578d7b06949ddb2fa6e57645c2e88d4b5411
yifeng96 <yifeng@berkeley.edu>
2015-03-12 11:47:21 -0700

    Completed lab8

--- lab8/Asymptotics.java
+++ lab8/Asymptotics.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.lang.Math;
 
 /** Class for testing the running times of various functions on
  *  different inputs. Usage:
@@ -28,6 +29,8 @@ public class Asymptotics {
                 case 4: function4(arg);
                     break;
                 case 5: function5(arg);
+                    break;
+                case 6: function6(arg);
                     break;
                 default: usage();
                         return;
@@ -76,13 +79,37 @@ public class Asymptotics {
     /** Fill in the body of this function so that its running time is
      *  n^(1/2) and so that it returns true if and only if n is prime. */
     public static boolean function4(long n) {
-	return false; //FIX ME
+        boolean flag = false;
+	    for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        flag = true;
+        return flag;
     }
 
     /** Fill in the body of this function so that its best case running
      *  time is log(n) and its worst case running time is n. */
     public static void function5(long n) {
-        //FILL ME IN
+        long j = 0;
+        long flag = 0;
+        for (long i = 2; i <= n; i *= 2) {
+            if (n == i) {
+                return;
+            }
+            flag = i; 
+        }
+        for (;j<n ;j++) {}
+    }
+
+    public static void function6(long n) {
+        for (long i = 0; i < n; i++) {
+            long j = i*i;
+            while (j <= n) {
+                j += 1;
+            }
+        }
     }
 
     public static void usage() {
