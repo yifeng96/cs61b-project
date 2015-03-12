d85e8b7fba6eecbc3d7725dd78d1f429ef3f0a45
yifeng96 <yifeng@berkeley.edu>
2015-03-12 01:07:45 -0700

    Completed proj1

--- proj1/ngordnet/NGramMap.java
+++ proj1/ngordnet/NGramMap.java
@@ -55,7 +55,7 @@ public class NGramMap {
     /** Returns a defensive copy of the YearlyRecord of WORD. */
     public YearlyRecord getRecord(int year) {
         HashMap<String, Integer> hMap = new HashMap<String, Integer>();
-        Iterator<Integer> xxx = yw.get(year).words.iterator();
+        Iterator<String> xxx = yw.get(year).words.iterator();
         while (xxx.hasNext()) {
             String in2 = xxx.next();
             hMap.put(in2,yw.get(year).count);
