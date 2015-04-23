19caf945581d5483c34a28b53d94877daad972c9
yifeng96 <yifeng@berkeley.edu>
2015-04-23 16:09:49 -0700

    lab13

--- lab13/MazeBreadthFirstPaths.java
+++ lab13/MazeBreadthFirstPaths.java
@@ -1,4 +1,4 @@
-import java.util.Observable;
+import java.util.*;
 /** 
  *  @author Josh Hug
  */
@@ -9,15 +9,50 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
-
+        private int s;
+    private int t;
+    private boolean targetFound = false;
+    private Maze maze; 
+    private static final int INFINITY = Integer.MAX_VALUE;
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
-        super(m);     
+                super(m);
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;   
     }
 
     /** Conducts a breadth first search of the maze starting at vertex x. */
     private void bfs(int s) {
-        /* Your code here. */
+
+        Queue<Integer> q = new Queue<Integer>();
+        for (int v = 0; v < maze.V(); v++) {
+            distTo[v] = INFINITY;
+        }
+        distTo[s] = 0;
+        marked[s] = true;
+        q.enqueue(s);
+
+        while (!q.isEmpty()) {
+            int v = q.dequeue();
+            announce();
+            for (int w : maze.adj(v)) {
+                if (!marked[w]) {
+                    edgeTo[w] = v;
+                    announce();
+                    distTo[w] = distTo[v] + 1;
+                    marked[w] = true;
+                    q.enqueue(w);
+                    if (targetFound) {
+                    return;
+                }              
+                }
+            }
+        }
     }
+
+
 
 
     @Override
