aa69eb3ed26936bd6c328d007488bf50c39203e1
yifeng96 <yifeng@berkeley.edu>
2015-04-28 00:43:03 -0700

    proj33

--- proj3/TST.java
+++ proj3/TST.java
@@ -1,147 +1,113 @@
-import java.util.*;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class TST<Value> {
-	private int N; // size
-	private Node<Value> root; // root of TST
+    private int N;              // size
+    private Node<Value> root;   // root of TST
 
-	private static class Node<Value> {
-		private char c; // character
-		private Node<Value> left, mid, right; // left, middle, and right
-												// subtries
-		private Value val; // value associated with string
-	}
+    private static class Node<Value> {
+        private char c;                        // character
+        private Node<Value> left, mid, right;  // left, middle, and right subtries
+        private Value val;                     // value associated with string
+    }
 
-	/**
-	 * Initializes an empty string symbol table.
-	 */
-	public TST() {
-	}
+    /**
+     * Initializes an empty string symbol table.
+     */
+    public TST() {
+    }
 
-	/**
-	 * Returns the number of key-value pairs in this symbol table.
-	 * 
-	 * @return the number of key-value pairs in this symbol table
-	 */
-	public int size() {
-		return N;
-	}
+    /**
+     * Returns the number of key-value pairs in this symbol table.
+     * @return the number of key-value pairs in this symbol table
+     */
+    public int size() {
+        return N;
+    }
 
-	/**
-	 * Does this symbol table contain the given key?
-	 * 
-	 * @param key
-	 *            the key
-	 * @return <tt>true</tt> if this symbol table contains <tt>key</tt> and
-	 *         <tt>false</tt> otherwise
-	 * @throws NullPointerException
-	 *             if <tt>key</tt> is <tt>null</tt>
-	 */
-	public boolean contains(String key) {
-		return get(key) != null;
-	}
+    /**
+     * Does this symbol table contain the given key?
+     * @param key the key
+     * @return <tt>true</tt> if this symbol table contains <tt>key</tt> and
+     *     <tt>false</tt> otherwise
+     * @throws NullPointerException if <tt>key</tt> is <tt>null</tt>
+     */
+    public boolean contains(String key) {
+        return get(key) != null;
+    }
 
-	/**
-	 * Returns the value associated with the given key.
-	 * 
-	 * @param key
-	 *            the key
-	 * @return the value associated with the given key if the key is in the
-	 *         symbol table and <tt>null</tt> if the key is not in the symbol
-	 *         table
-	 * @throws NullPointerException
-	 *             if <tt>key</tt> is <tt>null</tt>
-	 */
-	public Value get(String key) {
-		if (key == null)
-			throw new NullPointerException();
-		if (key.length() == 0)
-			throw new IllegalArgumentException("key must have length >= 1");
-		Node<Value> x = get(root, key, 0);
-		if (x == null)
-			return null;
-		return x.val;
-	}
+    /**
+     * Returns the value associated with the given key.
+     * @param key the key
+     * @return the value associated with the given key if the key is in the symbol table
+     *     and <tt>null</tt> if the key is not in the symbol table
+     * @throws NullPointerException if <tt>key</tt> is <tt>null</tt>
+     */
+    public Value get(String key) {
+        if (key == null) throw new NullPointerException();
+        if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
+        Node<Value> x = get(root, key, 0);
+        if (x == null) return null;
+        return x.val;
+    }
 
-	// return subtrie corresponding to given key
-	private Node<Value> get(Node<Value> x, String key, int d) {
-		if (key == null)
-			throw new NullPointerException();
-		if (key.length() == 0)
-			throw new IllegalArgumentException("key must have length >= 1");
-		if (x == null)
-			return null;
-		char c = key.charAt(d);
-		if (c < x.c)
-			return get(x.left, key, d);
-		else if (c > x.c)
-			return get(x.right, key, d);
-		else if (d < key.length() - 1)
-			return get(x.mid, key, d + 1);
-		else
-			return x;
-	}
+    // return subtrie corresponding to given key
+    private Node<Value> get(Node<Value> x, String key, int d) {
+        if (key == null) throw new NullPointerException();
+        if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
+        if (x == null) return null;
+        char c = key.charAt(d);
+        if      (c < x.c)              return get(x.left,  key, d);
+        else if (c > x.c)              return get(x.right, key, d);
+        else if (d < key.length() - 1) return get(x.mid,   key, d+1);
+        else                           return x;
+    }
 
-	/**
-	 * Inserts the key-value pair into the symbol table, overwriting the old
-	 * value with the new value if the key is already in the symbol table. If
-	 * the value is <tt>null</tt>, this effectively deletes the key from the
-	 * symbol table.
-	 * 
-	 * @param key
-	 *            the key
-	 * @param val
-	 *            the value
-	 * @throws NullPointerException
-	 *             if <tt>key</tt> is <tt>null</tt>
-	 */
-	public void put(String key, Value val) {
-		if (contains(key)) {
-			throw new IllegalArgumentException();
-		}
-		if (!contains(key))
-			N++;
-		root = put(root, key, val, 0);
-	}
+    /**
+     * Inserts the key-value pair into the symbol table, overwriting the old value
+     * with the new value if the key is already in the symbol table.
+     * If the value is <tt>null</tt>, this effectively deletes the key from the symbol table.
+     * @param key the key
+     * @param val the value
+     * @throws NullPointerException if <tt>key</tt> is <tt>null</tt>
+     */
+    public void put(String key, Value val) {
+        if (contains(key)) {
+            throw new IllegalArgumentException();
+        }
+        if (!contains(key)) N++;
+        root = put(root, key, val, 0);
+    }
 
-	private Node<Value> put(Node<Value> x, String key, Value val, int d) {
-		char c = key.charAt(d);
-		if (x == null) {
-			x = new Node<Value>();
-			x.c = c;
-		}
-		if (c < x.c)
-			x.left = put(x.left, key, val, d);
-		else if (c > x.c)
-			x.right = put(x.right, key, val, d);
-		else if (d < key.length() - 1)
-			x.mid = put(x.mid, key, val, d + 1);
-		else
-			x.val = val;
-		return x;
-	}
+    private Node<Value> put(Node<Value> x, String key, Value val, int d) {
+        char c = key.charAt(d);
+        if (x == null) {
+            x = new Node<Value>();
+            x.c = c;
+        }
+        if      (c < x.c)               x.left  = put(x.left,  key, val, d);
+        else if (c > x.c)               x.right = put(x.right, key, val, d);
+        else if (d < key.length() - 1)  x.mid   = put(x.mid,   key, val, d+1);
+        else                            x.val   = val;
+        return x;
+    }
+    public Queue<String> keysWithPrefix(String prefix) {
+        Queue<String> queue = new Queue<String>();
+        Node<Value> x = get(root, prefix, 0);
+        if (x == null) return queue;
+        if (x.val != null) queue.enqueue(prefix);
+        collect(x.mid, new StringBuilder(prefix), queue);
+        return queue;
+    }
 
-	public Queue<String> keysWithPrefix(String prefix) {
-		Queue<String> queue = new Queue<String>();
-		Node<Value> x = get(root, prefix, 0);
-		if (x == null)
-			return queue;
-		if (x.val != null)
-			queue.enqueue(prefix);
-		collect(x.mid, new StringBuilder(prefix), queue);
-		return queue;
-	}
-
-	// all keys in subtrie rooted at x with given prefix
-	private void collect(Node<Value> x, StringBuilder prefix,
-			Queue<String> queue) {
-		if (x == null)
-			return;
-		collect(x.left, prefix, queue);
-		if (x.val != null)
-			queue.enqueue(prefix.toString() + x.c);
-		collect(x.mid, prefix.append(x.c), queue);
-		prefix.deleteCharAt(prefix.length() - 1);
-		collect(x.right, prefix, queue);
-	}
+    // all keys in subtrie rooted at x with given prefix
+    private void collect(Node<Value> x, StringBuilder prefix, Queue<String> queue) {
+        if (x == null) return;
+        collect(x.left,  prefix, queue);
+        if (x.val != null) queue.enqueue(prefix.toString() + x.c);
+        collect(x.mid,   prefix.append(x.c), queue);
+        prefix.deleteCharAt(prefix.length() - 1);
+        collect(x.right, prefix, queue);
+    }
 
 }
