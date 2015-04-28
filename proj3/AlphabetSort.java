c338204bd6efea881322fb1f62d0424889a72b51
yifeng96 <yifeng@berkeley.edu>
2015-04-28 00:25:01 -0700

    proj33

--- proj3/AlphabetSort.java
+++ proj3/AlphabetSort.java
@@ -1,57 +1,58 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class AlphabetSort {
-	String output;
-	static String[] alphabet;
-	static Trie t;
-	static Node a;
+  String output;
+  static String[] alphabet;
+  static Trie t;
+  static Node a;
 
-	public static void main(String[] args) {
-		AlphabetSort as = new AlphabetSort();
-		Scanner input = new Scanner(System.in);
-		if (!input.hasNextLine()) {
-			throw new IllegalArgumentException();
-		}
-		String alph = input.nextLine();
-		alphabet = alph.split("(?<=\\G.{1})");
+  public static void main(String[] args) {
+    AlphabetSort as = new AlphabetSort();
+    Scanner input = new Scanner(System.in);
+    if (!input.hasNextLine()) {
+      throw new IllegalArgumentException();
+    }
+    String alph = input.nextLine();
+    alphabet = alph.split("(?<=\\G.{1})");
 
-		t = new Trie();
-		while (input.hasNextLine()) {
-			t.insert(input.nextLine());
-		}
-		HashMap<String, Integer> map = new HashMap();
-		for (int i = 0; i < alphabet.length; i++) {
-			map.put(alphabet[i], i);
-		}
-		a = t.root;
-		as.sort(a);
+    t = new Trie();
+    while (input.hasNextLine()) {
+      t.insert(input.nextLine());
+    }
+    HashMap<String, Integer> map = new HashMap();
+    for (int i = 0; i < alphabet.length; i++) {
+      map.put(alphabet[i], i);
+    }
+    a = t.root;
+    as.sort(a);
 
-	}
+  }
 
-	public void sort(Node x) {
+  public void sort(Node x) {
 
-		if (t.word.contains(x.output)) {
+    if (t.word.contains(x.output)) {
 
-			System.out.println(x.output);
+      System.out.println(x.output);
 
-			output = "";
+      output = "";
 
-		} else {
-			for (int p = 0; p < alphabet.length; p++) {
+    } else {
+      for (String element : alphabet) {
 
-				char c = alphabet[p].charAt(0);
+        char c = element.charAt(0);
 
-				if (x.links[c] != null) {
+        if (x.links[c] != null) {
 
-					sort(x.links[c]);
+          sort(x.links[c]);
 
-				}
-			}
-		}
-	}
+        }
+      }
+    }
+  }
 
-	public void sort2() {
+  public void sort2() {
 
-	}
+  }
 
 }
