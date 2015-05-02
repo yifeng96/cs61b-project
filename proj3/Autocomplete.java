6d32dd6557bd35142c5a95843a68eae9e1094747
yifeng96 <yifeng@berkeley.edu>
2015-05-02 06:17:04 -0700

    1

--- proj3/Autocomplete.java
+++ proj3/Autocomplete.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import edu.princeton.cs.introcs.In;
 
 /**
  * Implements autocomplete on prefixes for a given dictionary of terms and
@@ -13,7 +14,7 @@ public class Autocomplete {
 	 * @param weights
 	 *            Array of weights.
 	 */
-	private final int hahaha = 15;
+	private final int hahaha = 1;
 	static TST<Double> ts;
 	String[] termarray;
 	double[] weightarray;
@@ -56,42 +57,40 @@ public class Autocomplete {
 	 * @return Best (highest weight) matching string in the dictionary.
 	 */
 	public String topMatch(String prefix) {
-		Queue<String> queue = ts.keysWithPrefix(prefix);
-		PriorityQueue<String> pq = new PriorityQueue<String>(hahaha, comparator);
-		while (!queue.isEmpty()) {
-			String sk = queue.dequeue();
-			pq.add(sk);
-		}
-		return pq.peek();
-	}
-
-	/**
-	 * Returns the top k matching terms (in descending order of weight) as an
-	 * iterable. If there are less than k matches, return all the matching
-	 * terms.
-	 * 
-	 * @param prefix
-	 * @param k
-	 * @return
-	 */
-	public Iterable<String> topMatches(String prefix, int k) {
-		if (k < 1) {
-			throw new IllegalArgumentException();
-		}
-		Queue<String> queue = ts.keysWithPrefix(prefix);
-		PriorityQueue<String> pq = new PriorityQueue<String>(hahaha, comparator);
-		while (!queue.isEmpty()) {
-			String sk = queue.dequeue();
-			pq.add(sk);
-		}
-		PriorityQueue<String> mypq = new PriorityQueue(k, comparator);
-		for (int i = 0; i < k; i++) {
-			String str = pq.remove();
-			mypq.add(str);
-		}
-		return mypq;
-
-	}
+Queue<String> queue = ts.keysWithPrefix(prefix);
+PriorityQueue<String> pq = new PriorityQueue<String>(hahaha, comparator);
+while (!queue.isEmpty()) {
+String sk = queue.dequeue();
+pq.add(sk);
+}
+return pq.peek();
+}
+/**
+* Returns the top k matching terms (in descending order of weight) as an
+* iterable. If there are less than k matches, return all the matching
+* terms.
+*
+* @param prefix
+* @param k
+* @return
+*/
+public Iterable<String> topMatches(String prefix, int k) {
+if (k < 1) {
+throw new IllegalArgumentException();
+}
+Queue<String> queue = ts.keysWithPrefix(prefix);
+PriorityQueue<String> pq = new PriorityQueue<String>(hahaha, comparator);
+while (!queue.isEmpty()) {
+String sk = queue.dequeue();
+pq.add(sk);
+}
+PriorityQueue<String> mypq = new PriorityQueue(k, comparator);
+for (int i = 0; i < k; i++) {
+String str = pq.remove();
+mypq.add(str);
+}
+return mypq;
+}
 
 	public static Comparator<String> comparator = new Comparator<String>() {
 		public int compare(String x, String y) {
--- proj3/TST.java
+++ proj3/TST.java
@@ -8,7 +8,10 @@ public class TST<Value> {
     private static class Node<Value> {
         private char c;                        // character
         private Node<Value> left, mid, right;  // left, middle, and right subtries
-        private Value val;                     // value associated with string
+        private Value val;  
+        private Value max = null;        
+        private String word;
+                   // value associated with string
     }
 
     /**
@@ -50,9 +53,15 @@ public class TST<Value> {
         if (x == null) return null;
         return x.val;
     }
-
+    public Value getmax(String key) {
+        if (key == null) throw new NullPointerException();
+        if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
+        Node<Value> x = get(root, key, 0);
+        if (x == null) return null;
+        return x.max;
+    }
     // return subtrie corresponding to given key
-    private Node<Value> get(Node<Value> x, String key, int d) {
+    public Node<Value> get(Node<Value> x, String key, int d) {
         if (key == null) throw new NullPointerException();
         if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
         if (x == null) return null;
@@ -81,6 +90,16 @@ public class TST<Value> {
 
     private Node<Value> put(Node<Value> x, String key, Value val, int d) {
         char c = key.charAt(d);
+        try{
+        if (x.max == null) {
+            x.max = val;
+        }
+        if ((Double)val > (Double)x.max) {
+            x.max = val;
+        }}
+        catch(NullPointerException a){
+            
+        }
         if (x == null) {
             x = new Node<Value>();
             x.c = c;
@@ -88,9 +107,12 @@ public class TST<Value> {
         if      (c < x.c)               x.left  = put(x.left,  key, val, d);
         else if (c > x.c)               x.right = put(x.right, key, val, d);
         else if (d < key.length() - 1)  x.mid   = put(x.mid,   key, val, d+1);
-        else                            x.val   = val;
+        else  {                         x.val   = val;
+                                        x.word = key;
+        }
         return x;
     }
+    
     public Queue<String> keysWithPrefix(String prefix) {
         Queue<String> queue = new Queue<String>();
         Node<Value> x = get(root, prefix, 0);
@@ -99,9 +121,31 @@ public class TST<Value> {
         collect(x.mid, new StringBuilder(prefix), queue);
         return queue;
     }
+    public String topMatch(String prefix) {
+        Node<Value> x = get(root, prefix, 0);
+        if (x == null) return null;
+        if (x.mid == null) {
+            return prefix;
+        }
+        x=x.mid;
+        while(x.mid != null){
+            if (x.left.max == x.max) {
+                x = x.left;
+            }
+            if (x.right.max == x.max) {
+                x = x.right;
+            }
+            if (x.mid.max == x.max) {
+                x = x.mid;
+            }
+        }
+        String str = x.word;
+        x = null;
+        return str;
+    }
 
     // all keys in subtrie rooted at x with given prefix
-    private void collect(Node<Value> x, StringBuilder prefix, Queue<String> queue) {
+    public void collect(Node<Value> x, StringBuilder prefix, Queue<String> queue) {
         if (x == null) return;
         collect(x.left,  prefix, queue);
         if (x.val != null) queue.enqueue(prefix.toString() + x.c);
